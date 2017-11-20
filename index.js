73f3a55b7d596f74c83780cfc33ac1ac78e78bef
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-20 13:16:13 -0500

    sent code

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const express = require('express')
 const cookieParser = require("cookie-parser");
 const exphbs = require("express-handlebars");
 const fs = require('fs');
+const session = require("express-session");
+
 
 const app = express()
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 
+
+app.use(
+session({
+  secret: "123456",
+  resave: false,
+  saveUninitialized: true
+})
+);
+
+
 app.use(cookieParser());
 
+
+app.get('/', (req,res) => {
+if (req.session.visited) {
+  res.render('main', params)
+} else {
+
+  req.session.visited = true;
+  res.render('main')
+};
+
+
+
+
+})
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
